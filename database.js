0d20216ab85ae2a9a62b097f1c803ae9e5e091af
unknown <yotta.rrb@gmail.com>
2025-02-19 13:14:10 +0530

    Fixed bcrypt issue

--- database.js
+++ database.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'manjeet'
+    host: 'https://www.uniworldvision.com', // Linode server ka IP ya domain
+    user: 'uniworldvision', // MySQL username
+    password: 'Uniworldvision@2023', // MySQL password
+    database: 'uniworldvision', // MySQL database name
+    port: 3306 // MySQL default port
 });
 
 async function initializeDatabase() {
